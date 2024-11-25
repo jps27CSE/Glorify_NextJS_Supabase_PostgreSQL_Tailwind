f3252f81ee10c6379b89e9b9d1cc9ddaf180500c
jps27CSE <jackpritomsoren@hotmail.com>
2024-11-25 15:58:51 +0600

    Added Sidebar and Main Component (In Progress)

---
+++ components/Header.tsx
@@ -0,0 +1,45 @@
+"use client";
+import React from "react";
+import { twMerge } from "tailwind-merge";
+import { useRouter } from "next/navigation";
+import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
+interface HeaderProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+const Header: React.FC<HeaderProps> = ({ children, className }) => {
+  const router = useRouter();
+
+  const handleLogout = () => {
+    // Handle logout s
+  };
+
+  return (
+    <div
+      className={twMerge(
+        `h-fit bg-gradient-to-b from-blue-900 p-6
+  `,
+        className,
+      )}
+    >
+      <div className="w-full mb-4 flex items-center justify-between">
+        <div className="hidden md:flex gap-x-2 items-center">
+          <button
+            onClick={() => router.back()}
+            className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition"
+          >
+            <RxCaretLeft className="text-white" size={35} />
+          </button>
+          <button
+            onClick={() => router.forward()}
+            className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition"
+          >
+            <RxCaretRight className="text-white" size={35} />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Header;
