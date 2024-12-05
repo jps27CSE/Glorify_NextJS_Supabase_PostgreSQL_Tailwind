d2764fc2a3637a600d7411a5e8e7e04948c47e33
jps27CSE <jackpritomsoren@hotmail.com>
2024-12-05 19:42:00 +0600

    Song Displaying on Library and Main Page

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -26,7 +26,6 @@ export default async function Home() {
           <h1 className="text-white text-2xl font-semibold"> Newest songs </h1>
         </div>
         <PageContent songs={songs} />
-        <div>List of Songs!</div>
       </div>
     </div>
   );
