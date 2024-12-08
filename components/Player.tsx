ae146a01707f2a232063a8531d384199d7075ddb
jps27CSE <jackpritomsoren@hotmail.com>
2024-12-08 16:02:21 +0600

    Song Playing (Player Functionality)

--- components/Player.tsx
+++ components/Player.tsx
@@ -3,6 +3,7 @@
 import usePlayer from "@/hooks/usePlayer";
 import useGetSongById from "@/hooks/useGetSongById";
 import useLoadSongUrl from "@/hooks/useLoadSongUrl";
+import PlayerContent from "@/components/PlayerContent";
 
 const Player = () => {
   const player = usePlayer();
@@ -16,7 +17,7 @@ const Player = () => {
 
   return (
     <div className="fixed bottom-0 bg-black w-full py-2 h-[80px] px-4">
-      Player
+      <PlayerContent key={songUrl} song={song} songUrl={songUrl} />
     </div>
   );
 };
